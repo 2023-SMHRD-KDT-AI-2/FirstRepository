bcfb021eedec650262a73a385c8a40f08b382d41
SM2312AA <SM2312AA@59.3.58.96>
2023-09-07 17:25:23 +0900

    나래 commit1

---
+++ playwithme/src/main/java/playwithme/goMain.java
@@ -0,0 +1,25 @@
+package playwithme;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class goMain
+ */
+public class goMain extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/MapMain.jsp");
+	rd.forward(request, response);
+	
+	}
+
+}
